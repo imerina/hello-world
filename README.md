48b9c8715d144de2661ee944b10a28cf64053f36
imerina <imerina@nomail.com>
2017-06-09 18:35:28 +0200

    Test 3
    Ajout d'une page index.php

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Ceci est une modification du fichier dans la branche readme-edits
 Ici un ajout depuis depuis ma version locale
 
 Ici une seconde ajout depuis ma version locale
+
+On a ajouté un fichier index.php
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />  
+  <meta http-equiv="Content-language" content="fr" />
+  <meta name="description" content="page d'accueil de Hello World" />  
+  <meta name="keywords" content="imerina, github, test" />
+  <meta name="author" content="imerina" />
+  <meta name="copyright" content="jean-francois Ramiara" />
+  <title>page d'accueil de Hello World</title>
+   <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+<?php
+    echo ("Bonjour le monde");
+?>
+</body>
+</html>
